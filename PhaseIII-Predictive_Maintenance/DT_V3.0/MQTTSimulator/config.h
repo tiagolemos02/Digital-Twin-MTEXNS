9151244d743c6cd3dc0ff6b9806761292fbd4451
Tiago Lemos <100294698+tiagolemos02@users.noreply.github.com>
2026-07-14 18:09:29 +0100

    #6.1.1.1: Update cred in config.h

--- PhaseIII-Predictive_Maintenance/DT_V3.0/MQTTSimulator/config.h
+++ PhaseIII-Predictive_Maintenance/DT_V3.0/MQTTSimulator/config.h
@@ -2,8 +2,8 @@
 #define CONFIG_H
 
 // Wi-Fi Configuration
-#define WIFI_SSID "Jorge_Mesh"
-#define WIFI_PASSWORD "antjorgetavlemosvsc1922"
+#define WIFI_SSID "some_ssid"
+#define WIFI_PASSWORD "some_pass"
 
 // MQTT Configuration
 #define MQTT_SERVER "192.168.68.63"
